37549cd0ec47d89d5d84d5e29d6f989cca3a1f1f
Noah Zinn <noahwzinn@gmail.com>
2025-07-25 14:41:27 -0500

    Refactored SPI_instructions with SPI_send_rec

--- src/spi_instructions.c
+++ src/spi_instructions.c
@@ -9,24 +9,33 @@
 #include <util/delay.h>
 
 
+
+
+
+/* Send a 4-byte instruction, putting the response  */
+static uint8_t SPI_send_rec(const uint8_t instruction[], uint8_t *response) {
+  SPI_master_tx(instruction[0]);
+  SPI_master_tx(instruction[1]);
+  SPI_master_tx(instruction[2]);
+
+  uint8_t echo = SPI_master_rx();
+  SPI_master_tx(instruction[3]);
+
+  if (response) {
+    *response = SPI_master_rx();
+  }
+
+  return echo == instruction[1];
+} 
+
+
+
 /* Enables programming mode on chip, signals if synchronization was achieved. */
 uint8_t SPI_prog_enable() {
 
+  const uint8_t inst[] = {0xAC, 0x53, 0x00, 0x00};
   
-  SPI_master_tx(0xAC);
-  SPI_master_tx(0x53);
-  SPI_master_tx(0x00);
-
-  uint8_t echo = SPI_master_rx();
-
-  SPI_master_tx(0x00);
-
-  /* If slave echoes back signal all good */
-  if (echo == 0x53)  {
-    return 1;
-  }
-
-  return 0;
+  return SPI_send_rec(inst, 0);
 }
 
 
@@ -34,23 +43,25 @@ uint8_t SPI_prog_enable() {
 
 /* Check if the chip is ready for another instruction */
 static uint8_t SPI_poll_ready() {
-  SPI_master_tx(0xF0);
-  SPI_master_tx(0x00);
-  SPI_master_tx(0x00);
-  SPI_master_tx(0x00);
+
+  const uint8_t inst[] = {0xF0, 0x00, 0x00, 0x00};
+
+  uint8_t rec;
+
+  SPI_send_rec(inst, &rec);
+
 
   /* Only lsb matters */
-  return !(SPI_master_rx() & 1);
+  return !(rec & 1);
 }
 
 
 /* Erases program memory and EEPROM to 0xFF */
 void SPI_chip_erase() {
-  SPI_master_tx(0xAC);
-  SPI_master_tx(0x80);
-  SPI_master_tx(0x00);
-  SPI_master_tx(0x00);
 
+  const uint8_t inst[] = {0xAC, 0x80, 0x00, 0x00};
+
+  SPI_send_rec(inst, 0);
   /* Wait for chip erase */
   while (!SPI_poll_ready()) {
       _delay_ms(1);
@@ -61,48 +72,45 @@ void SPI_chip_erase() {
 /* Loads data into address[5:0] of page buffer*/
 void SPI_write_flash_addr(uint8_t addr_lsb, uint8_t data_lsb, uint8_t data_msb)  {
 
-  /* Load low data byte */
-  SPI_master_tx(0x40);
-  SPI_master_tx(0x00);
-  SPI_master_tx( addr_lsb);
-  SPI_master_tx( data_lsb);
 
-  /* Load high data byte */
-  SPI_master_tx(0x48);
-  SPI_master_tx(0x00);
-  SPI_master_tx(addr_lsb);
-  SPI_master_tx(data_msb);
+  const uint8_t low_inst[] = {0x40, 0x00, addr_lsb, data_lsb};
+  const uint8_t high_inst[] = {0x48, 0x00, addr_lsb, data_msb};
+  /* Load low byte */
+  SPI_send_rec(low_inst, 0);
+  /* Load high byte */
+  SPI_send_rec(high_inst, 0);
 }
 
 
 uint8_t SPI_read_flash_addr_high(uint8_t addr_lsb, uint8_t addr_msb) {
-  SPI_master_tx(0x28);
-  SPI_master_tx(addr_msb);
-  SPI_master_tx(addr_lsb);
-  SPI_master_tx(0x0);
 
-  return SPI_master_rx();
+  const uint8_t inst[] = {0x28, addr_msb, addr_lsb, 0x00};
+  uint8_t rec;
+
+  SPI_send_rec(inst, &rec);
+  return rec;
   
 }
 
 uint8_t SPI_read_flash_addr_low(uint8_t addr_lsb, uint8_t addr_msb) {
-  SPI_master_tx(0x20);
-  SPI_master_tx(addr_msb);
-  SPI_master_tx(addr_lsb);
-  SPI_master_tx(0x0);
 
-  return SPI_master_rx();
+  const uint8_t inst[] = {0x20, addr_msb, addr_lsb, 0x00};
   
+
+  uint8_t rec;
+  SPI_send_rec(inst, &rec);
+
+  return rec;
 }
 
 
 /* write the page of address[13:6] */
 void SPI_write_flash_page(uint8_t addr_lsb, uint8_t addr_msb) {
-  SPI_master_tx(0x4c);
-  SPI_master_tx(addr_msb);
-  SPI_master_tx(addr_lsb);
-  SPI_master_tx(0x00);
 
+  const uint8_t inst[] = {0x4c, addr_msb, addr_lsb, 0x00};
+
+  SPI_send_rec(inst, 0);
+  
   /* Wait for page write */
   while(!SPI_poll_ready()) {
     _delay_ms(1);
